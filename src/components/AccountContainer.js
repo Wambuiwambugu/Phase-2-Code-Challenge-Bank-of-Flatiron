533c5bdbe492e4bdde3bf95901a9a6563753365f
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-06-04 12:21:50 +0300

    display transaction list

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactionData, setTransactionData] = useState([]);
+  const [newTransaction, setNewTransaction] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+    .then(res => res.json())
+    .then(data => setTransactionData(data))
+  },[]);
+  
+  function handleAddTransaction(formEntry){
+    setNewTransaction(formEntry);
+    setTransactionData([...transactionData,newTransaction])
+   
+    
+  }
+
   return (
     <div>
       <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <AddTransactionForm addTransaction={handleAddTransaction} newId={transactionData.length} />
+      <TransactionsList transactions={transactionData}/>
     </div>
   );
 }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({addTransaction, newId}) {
+  const [formData, setFormData] = useState({
+    id: 0,
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  });
+
+  function handleChange(e){
+    const name = e.target.name
+    const value = e.target.value
+    formData.id = newId + 1
+    setFormData({...formData,[name]: value});
+    
+    
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    
+    alert(`id is ${formData.id}, date is ${formData.date}, description is ${formData.description}, category is ${formData.category}, amount is ${formData.amount}`);
+    
+    addTransaction(formData)
+    
+
+    
+  }
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" required onChange={handleChange}/>
+          <input type="text" name="description" placeholder="Description" required onChange={handleChange}/>
+          <input type="text" name="category" placeholder="Category" required onChange={handleChange}/>
+          <input type="number" name="amount" placeholder="Amount" step="0.01" required onChange={handleChange}/>
         </div>
         <button className="ui button" type="submit">
           Add Transaction
